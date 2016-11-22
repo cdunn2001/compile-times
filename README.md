d2a214052c0bb3eb185173c8bdd6d8e3d301cefb
Christopher Dunn <cdunn2001@gmail.com>
2016-11-21 18:57:59 -0600

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.reddit.com/r/rust/comments/55k577/rust_compilation_times_compared_to_c_d_go_pascal/
---
+++ driver.py
@@ -0,0 +1,70 @@
+# http://pastie.org/pastes/10943133/text
+# Copyright (c) 2016 1wd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import timeit
+import subprocess
+
+def do(args):
+    start = timeit.default_timer()
+    subprocess.call(args)
+    end = timeit.default_timer()
+    return end - start
+
+langs = [
+    ("VC++", "cpp", ["cl.exe", "/nologo", "/Od"], False),
+    ("Clang", "cpp", ["clang++.exe", "-O0"], False),
+    ("D", "d", ["dmd.exe"], False),
+    ("Go", "go", ["go.exe", "build", "-gcflags", '-N -l'], False),
+    ("Pascal", "pas", ["fpc.exe", "-v0", "-O-"], False),
+    ("Rust", "rs", ["rustc.exe", "-C", "opt-level=0"], False),
+
+    ("VC++ (opt)", "cpp", ["cl.exe", "/nologo", "/O2"], True),
+    ("Clang (opt)", "cpp", ["clang++.exe", "-O2"], True),
+    ("D (opt)", "d", ["dmd.exe", "-O"], True),
+    ("Go (opt)", "go", ["go.exe", "build"], True),
+    ("Pascal (opt)", "pas", ["fpc.exe", "-v0", "-O2"], True),
+    ("Rust (opt)", "rs", ["rustc.exe", "-C", "opt-level=2"], True),
+]
+
+num_seeds = 1
+n = 10
+n_step = 1000
+
+opt_levels = [
+    True,
+    True,
+]
+
+results = []
+
+for i in range(n):
+    num_funcs = n_step * (i+1)
+    print "Generate code..."
+    for seed in range(num_seeds):
+        do(["c:\python27\python.exe", "..\cogen.py",
+            str(seed), str(num_funcs)])
+
+    row = [num_funcs]
+    for lang, ext, args, opt in langs:
+        if opt not in opt_levels: continue
+        print "%s..." % lang
+        t = 0
+        for seed in range(num_seeds):
+            filename = 'test_%s_s%s_n%s.%s' % (ext, seed, num_funcs, ext)
+            t += do(args + [filename])
+        row.append(t / num_seeds)
+    results.append(row)
+
+print "Create CSV..."
+with open("results.csv", "w") as f:
+    f.write(','.join(["n"] + [lang[0] for lang in langs]))
+    f.write('\n')
+    for row in results:
+        f.write(','.join(str(x) for x in row))
+        f.write('\n')
