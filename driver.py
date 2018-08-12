8006490b52608a731bb3bc7a3c0e821e81525feb
Christopher Dunn <cdunn2001@gmail.com>
2018-08-12 13:44:57 -0500

    Try opt too

--- driver.py
+++ driver.py
@@ -28,12 +28,13 @@ langs = [
     ("Rust", "rs", ["rustc", "-C", "opt-level=0"], False),
 
     ##("VC++ (opt)", "cpp", ["cl.exe", "/nologo", "/O2"], True),
-    #("Clang++ (opt)", "cpp", ["clang++", "-O2"], True),
-    #("Clang (opt)", "c", ["clang", "-O2"], True),
-    ##("D (opt)", "d", ["dmd", "-O"], True),
-    #("Go (opt)", "go", ["go", "build"], True),
+    ("Nim (opt)", "nim", ["nim", "c", "-d:release"], False),
+    ("Clang++ (opt)", "cpp", ["clang++", "-O2"], True),
+    ("Clang (opt)", "c", ["clang", "-O2"], True),
+    #("D (opt)", "d", ["dmd", "-O"], True),
+    ("Go (opt)", "go", ["go", "build"], True),
     ##("Pascal (opt)", "pas", ["fpc.exe", "-v0", "-O2"], True),
-    #("Rust (opt)", "rs", ["rustc", "-C", "opt-level=2"], True),
+    ("Rust (opt)", "rs", ["rustc", "-C", "opt-level=2"], True),
 ]
 
 num_seeds = 1
